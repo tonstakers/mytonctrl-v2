19e1c568253d32e23bb85760bc14e277d69e950c
dl@tonstakers.com <dl@tonstakers.com>
2024-08-09 16:42:23 +0300

    support remove custom folder unisntall.sh

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -40,6 +40,9 @@ if $full; then
 	rm -rf /var/ton-dht-server
 fi
 
+# tonstakers - todo remove
+rm -rf /usr/src/mytonctrl-v2
+####
 rm -rf /usr/src/mytonctrl
 rm -rf /usr/src/mtc-jsonrpc
 rm -rf /usr/src/pytonv3
